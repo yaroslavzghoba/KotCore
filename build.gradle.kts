3a9b9b3855d59deb03ca1f1ce2006f2625424813
yaroslavzghoba <yaroslavzghoba@gmail.com>
2026-05-30 16:51:02 +0200

    [feat] Create a foundation for the KMP library.

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply  false
+    alias(libs.plugins.vanniktech.maven.publish) apply false
+}
